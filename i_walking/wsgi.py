f34aa8eb3a03997db475ad3d29afd30234ec748f
b.khasanov <afti@yandex.ru>
2016-06-17 00:25:23 +0300

    init

---
+++ i_walking/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for i_walking project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "i_walking.settings")
+
+application = get_wsgi_application()
