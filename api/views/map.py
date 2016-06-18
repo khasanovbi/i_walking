36203c70fa245b68a2d090011389d1e9627aaa50
b.khasanov <afti@yandex.ru>
2016-06-18 10:29:38 +0300

    Дал права всем на просмотр роутов

--- api/views/map.py
+++ api/views/map.py
@@ -1,14 +1,17 @@
 import requests
 from django.conf import settings
 from rest_framework import views
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 
 class RouteView(views.APIView):
+    permission_classes = (AllowAny,)
+
     def get(self, request, format=None):
         return Response(
             requests.get(
                 'http://catalog.api.2gis.ru/2.0/region/list?key={key}'
-                .format(key=settings.DOUBLE_GIS_API_KEY)
+                    .format(key=settings.DOUBLE_GIS_API_KEY)
             ).json()['result']
         )
