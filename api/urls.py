8fe074479f5422057c44c0295cb871fc3cfeb863
b.khasanov <afti@yandex.ru>
2016-06-18 12:20:32 +0300

    Заготовки для других View

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,19 @@
 from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
 
-from api.views import RandomRouteView
+from api.views import *
 from api.views.auth import UserViewSet
 
 router = DefaultRouter()
 router.register('user', UserViewSet)
 
 route_urlpatterns = [
+    url('^investigate', InvestigateRouteView.as_view()),
+    url('^food', FoodRouteView.as_view()),
+    url('^bar', BarRouteView.as_view()),
+    url('^culture', CultureRouteView.as_view()),
+    url('^romantic', RomanticRouteView.as_view()),
     url('^random', RandomRouteView.as_view()),
-    # url('^random', RouteView.as_view()),
-    # url('^random', RouteView.as_view()),
-    # url('^random', RouteView.as_view()),
 ]
 
 map_urlpatterns = [
--- api/views/map.py
+++ api/views/map.py
@@ -7,6 +7,31 @@ from rest_framework.response import Response
 from api.serializers.map.route import InputRouteSerializer
 
 
+class AbstractRouteView(generics.GenericAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = InputRouteSerializer
+
+
+class InvestigateRouteView(AbstractRouteView):
+    pass
+
+
+class FoodRouteView(AbstractRouteView):
+    pass
+
+
+class BarRouteView(AbstractRouteView):
+    pass
+
+
+class CultureRouteView(AbstractRouteView):
+    pass
+
+
+class RomanticRouteView(AbstractRouteView):
+    pass
+
+
 class RandomRouteView(generics.GenericAPIView):
     permission_classes = (AllowAny,)
     serializer_class = InputRouteSerializer
@@ -20,9 +45,9 @@ class RandomRouteView(generics.GenericAPIView):
             '&type=street,building'
             '&radius={radius}'
             '&key={key}'
-                .format(
+            .format(
                 key=settings.DOUBLE_GIS_API_KEY,
-                radius=200,
+                radius=250,
                 longitude=serializer.data['longitude'],
                 latitude=serializer.data['latitude']
             )
